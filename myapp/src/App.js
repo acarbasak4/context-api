c06d29ae95d78ec96f2f327a533e802773af40b2
Başak Acar <acarbasak4@gmail.com>
2020-12-29 00:52:31 +0300

    -

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,35 +1,40 @@
 import React from "react";
 
-
 const themes = {
-light:{
-  foreground: '#000000',
-  background: '#eeeeee',
-},
-dark: {
-  foreground: '#ffffff',
-  background: '#222222',
-},
-}
+  light: {
+    foreground: "#000000",
+    background: "#eeeeee",
+  },
+  dark: {
+    foreground: "#ffffff",
+    background: "#222222",
+  },
+};
 
 const ThemeContext = React.createContext(themes.dark);
 
-const ThemedButton = () =>{
-  return(
-    <button>Themed Button</button>
+const ThemedButton = () => {
+  return( 
+  <ThemeContext.Consumer>
+  {
+    value => (
+      <button>Themed Button {value.background} </button>
+    )
+  } 
+  </ThemeContext.Consumer>
   );
-}
+};
 
-const Toolbar = () => (
-  <ThemedButton/>
-);
+const Toolbar = () => <ThemedButton />;
 
 class App extends React.Component {
   render() {
-    return( 
-    <div className="App">
-    <Toolbar/>
-    </div>
+    return (
+      <div className="App">
+        <ThemeContext.Provider value={themes.dark}>
+          <Toolbar />
+        </ThemeContext.Provider>
+      </div>
     );
   }
 }
