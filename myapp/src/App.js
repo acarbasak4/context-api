16e04ca5d915f05627c2c9125114086f4bbda8d1
Başak Acar <acarbasak4@gmail.com>
2020-12-28 22:07:21 +0300

    -

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,10 +1,37 @@
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-    
-    </div>
+
+const themes = {
+light:{
+  foreground: '#000000',
+  background: '#eeeeee',
+},
+dark: {
+  foreground: '#ffffff',
+  background: '#222222',
+},
+}
+
+const ThemeContext = React.createContext(themes.dark);
+
+const ThemedButton = () =>{
+  return(
+    <button>Themed Button</button>
   );
+}
+
+const Toolbar = () => (
+  <ThemedButton/>
+);
+
+class App extends React.Component {
+  render() {
+    return( 
+    <div className="App">
+    <Toolbar/>
+    </div>
+    );
+  }
 }
 
 export default App;
